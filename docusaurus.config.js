dedea289b639e2f110bb0ea179622c134477beb2
redhat123456 <1907065810@qq.com>
2021-11-30 12:59:30 +0800

    update js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Anview box',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://tanger.ltd/miniweb2/',
+  baseUrl: '/miniweb2/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'anview', // Usually your GitHub org/user name.
+  projectName: 'miniweb', // Usually your repo name.
 
   presets: [
     [
